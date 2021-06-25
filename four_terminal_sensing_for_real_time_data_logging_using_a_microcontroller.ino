eec3a2cd6e31628f570fb76d29a9affb14deecda
Keegan Green <39037966+keeganmjgreen@users.noreply.github.com>
2021-06-24 17:25:12 -0700

    Update four_terminal_sensing_for_real_time_data_logging_using_a_microcontroller.ino

--- four_terminal_sensing_for_real_time_data_logging_using_a_microcontroller.ino
+++ four_terminal_sensing_for_real_time_data_logging_using_a_microcontroller.ino
@@ -6,13 +6,13 @@ void setup()
 }
 void loop()
 {
-  float V_1 = analogRead(1);
-  float V_2 = analogRead(2);
-  float V_3 = analogRead(3);
+  float V_1 = analogRead(1); // Measured voltage across series resistor.
+  float V_2 = analogRead(2); // Measured voltage looking into the sensor at terminal `T2`.
+  float V_3 = analogRead(3); // Measured voltage looking into the sensor at terminal `T3`.
 
-  float I = V_1 / R_series;
-  float V_elem = V_3 - V_2;
-  float R_elem = V_elem / I;
+  float I = V_1 / R_series;  // Calculated current through both series resistor and sensor.
+  float V_elem = V_3 - V_2;  // Calculated voltage across sensing element.
+  float R_elem = V_elem / I; // Calculated resistance of sensing element.
 
   Serial.print("R_elem:");
   Serial.print(R_elem);
@@ -21,7 +21,7 @@ void loop()
   Serial.print("lower:");
   Serial.print(0);
   Serial.print("\t");
-
+ 
   Serial.print("upper:");
   Serial.print(10e3);
   Serial.print("\n");
