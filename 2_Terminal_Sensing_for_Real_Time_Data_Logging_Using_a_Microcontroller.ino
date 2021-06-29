9cd7ca172feb1c8ae9252c2755624d6afc61dc25
Keegan Green <39037966+keeganmjgreen@users.noreply.github.com>
2021-06-29 12:45:46 -0700

    Add files via upload

--- 2_Terminal_Sensing_for_Real_Time_Data_Logging_Using_a_Microcontroller.ino
+++ 2_Terminal_Sensing_for_Real_Time_Data_Logging_Using_a_Microcontroller.ino
@@ -8,25 +8,25 @@ float R_sens;
 
 void setup()
 {
-  Serial.begin(9600);   // Open communication with a computer via USB or with another device via UART.
+    Serial.begin(9600);   // Open communication with a computer via USB or with another device via UART.
 }
 void loop()
 {
-  V_1 = analogRead(1);  // Measured voltage across series resistor.
-
-  I = V_1 / R_series;   // Calculated current through both series resistor and 2-terminal sensor.
-  V_sens = 5 - V_1;     // Calculated voltage across 2-terminal sensor.
-  R_sens = V_sens / I;  // Calculated resistance of 2-terminal sensor.
-
-  Serial.print("R_sens:");
-  Serial.print(R_sens);
-  Serial.print("\t");
-
-  Serial.print("lower:");
-  Serial.print(0);
-  Serial.print("\t");
- 
-  Serial.print("upper:");
-  Serial.print(10e3);
-  Serial.print("\n");
+    V_1 = analogRead(1);  // Measured voltage across series resistor.
+    
+    I = V_1 / R_series;   // Calculated current through both series resistor and 2-terminal sensor.
+    V_sens = 5 - V_1;     // Calculated voltage across 2-terminal sensor.
+    R_sens = V_sens / I;  // Calculated resistance of 2-terminal sensor.
+    
+    Serial.print("R_sens:");
+    Serial.print(R_sens);
+    Serial.print("\t");
+    
+    Serial.print("lower:");
+    Serial.print(0);
+    Serial.print("\t");
+    
+    Serial.print("upper:");
+    Serial.print(10e3);
+    Serial.print("\n");
 }
--- 4_Terminal_Sensing_for_Real_Time_Data_Logging_Using_a_Microcontroller.ino
+++ 4_Terminal_Sensing_for_Real_Time_Data_Logging_Using_a_Microcontroller.ino
@@ -10,27 +10,27 @@ float R_elem;
 
 void setup()
 {
-  Serial.begin(9600);   // Open communication with a computer via USB or with another device via UART.
+    Serial.begin(9600);   // Open communication with a computer via USB or with another device via UART.
 }
 void loop()
 {
-  V_1 = analogRead(1);  // Measured voltage across series resistor.
-  V_2 = analogRead(2);  // Measured voltage looking into the 4-terminal sensor at terminal `T2`.
-  V_3 = analogRead(3);  // Measured voltage looking into the 4-terminal sensor at terminal `T3`.
-
-  I = V_1 / R_series;   // Calculated current through both series resistor and 4-terminal sensor.
-  V_elem = V_3 - V_2;   // Calculated voltage across sensing element.
-  R_elem = V_elem / I;  // Calculated resistance of sensing element.
-
-  Serial.print("R_elem:");
-  Serial.print(R_elem);
-  Serial.print("\t");
-
-  Serial.print("lower:");
-  Serial.print(0);
-  Serial.print("\t");
- 
-  Serial.print("upper:");
-  Serial.print(10e3);
-  Serial.print("\n");
+    V_1 = analogRead(1);  // Measured voltage across series resistor.
+    V_2 = analogRead(2);  // Measured voltage looking into the 4-terminal sensor at terminal `T2`.
+    V_3 = analogRead(3);  // Measured voltage looking into the 4-terminal sensor at terminal `T3`.
+    
+    I = V_1 / R_series;   // Calculated current through both series resistor and 4-terminal sensor.
+    V_elem = V_3 - V_2;   // Calculated voltage across sensing element.
+    R_elem = V_elem / I;  // Calculated resistance of sensing element.
+    
+    Serial.print("R_elem:");
+    Serial.print(R_elem);
+    Serial.print("\t");
+    
+    Serial.print("lower:");
+    Serial.print(0);
+    Serial.print("\t");
+    
+    Serial.print("upper:");
+    Serial.print(10e3);
+    Serial.print("\n");
 }
