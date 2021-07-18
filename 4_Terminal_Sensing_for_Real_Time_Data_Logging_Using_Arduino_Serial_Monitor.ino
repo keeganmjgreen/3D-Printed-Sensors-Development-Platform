ed1d9323dc7a92ef1abf72f67a87dbc9243bd778
Keegan Green <39037966+keeganmjgreen@users.noreply.github.com>
2021-07-18 16:04:39 -0700

    Create 4_Terminal_Sensing_for_Real_Time_Data_Logging_Using_Arduino_Serial_Monitor.ino

---
+++ 4_Terminal_Sensing_for_Real_Time_Data_Logging_Using_Arduino_Serial_Monitor.ino
@@ -0,0 +1,26 @@
+float R_series = 10e3;  // Known value of resistor in series with 4-terminal sensor.
+
+float V_1;
+float V_2;
+float V_3;
+
+float I;
+float V_elem;
+float R_elem;
+
+void setup()
+{
+    Serial.begin(9600);   // Open communication with a computer via USB or with another device via UART.
+}
+void loop()
+{
+    V_1 = analogRead(1);  // Measured voltage across series resistor.
+    V_2 = analogRead(2);  // Measured voltage looking into the 4-terminal sensor at terminal `T2`.
+    V_3 = analogRead(3);  // Measured voltage looking into the 4-terminal sensor at terminal `T3`.
+    
+    I = V_1 / R_series;   // Calculated current through both series resistor and 4-terminal sensor.
+    V_elem = V_3 - V_2;   // Calculated voltage across sensing element.
+    R_elem = V_elem / I;  // Calculated resistance of sensing element.
+    
+    Serial.println(R_elem);
+}
